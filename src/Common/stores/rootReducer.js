a9b0bfb2d40e4df70588f25a4070f394106d89b9
Pavan g <pavangangireddy@ibhubs.co>
2020-07-09 16:09:20 +0530

    removed comments

--- src/Common/stores/rootReducer.js
+++ src/Common/stores/rootReducer.js
@@ -1,15 +1,9 @@
 import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
 import TodoReducer from "./TodoStore/TodoReducer";
-// import RequestingReducer from "./requesting/RequestingReducer";
-// import ErrorReducer from "./error/ErrorReducer";
-// import ShowsReducer from "./shows/ShowsReducer";
-// import ToastsReducer from "./toasts/ToastsReducer";
 
 export default history => {
   const reducerMap = {
-    // error: ErrorReducer.reducer,
-    // requesting: RequestingReducer.reducer,
     router: connectRouter(history),
     todoStore: TodoReducer
   };
